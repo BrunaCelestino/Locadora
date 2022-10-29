b596878df3c61edc6311fe70d3aeb24cbcb3fbb1
BrunaMassuchini <brunaccelestino@gmail.com>
2022-10-28 23:21:43 -0300

    feat: add cd class

---
+++ src/CD.java
@@ -0,0 +1,34 @@
+public class CD extends Midia{
+    private String artista;
+    private int quantidadeFaixas;
+
+    public CD(String codigo, Double preco, String nome, String artista, int quantidadeFaixas) {
+        super(codigo, preco, nome);
+        this.artista = artista;
+        this.quantidadeFaixas = quantidadeFaixas;
+    }
+
+
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public int getQuantidadeFaixas() {
+        return quantidadeFaixas;
+    }
+
+    public void setQuantidadeFaixas(int quantidadeFaixas) {
+        this.quantidadeFaixas = quantidadeFaixas;
+    }
+
+
+    @Override
+    public void darPlay() {
+        System.out.println("Primeira faixa tocando");
+    }
+}
--- src/DoacaoMidia.java
+++ src/DoacaoMidia.java
@@ -11,14 +11,20 @@ public class DoacaoMidia {
         Dvd dvd1 = new Dvd ("0001",10.00, "Os Vingadores", "Português", true, "Inglês");
         Dvd dvd2 = new Dvd ("0002",12.00, "O Discurso do Rei", "Português", true, "Inglês");
         Dvd dvd3 = new Dvd ("0003",9.00, "Minha Mãe é Uma Peça", "Português", false, "Português");
+        CD cd1 = new CD("0004",5.00, "Circus", "Britney Spears", 20);
+        CD cd2 = new CD("0005",15.00, "Punk in Drublic", "NOFX", 17);
+        CD cd3 = new CD("0005",19.00, "Ritmo Perfeito", "Annita", 15);
 
         Random gerador = new Random();
-        int aleatorio = gerador.nextInt(3);
+        int aleatorio = gerador.nextInt(6);
 
         switch (aleatorio){
             case 0: return dvd1;
             case 1: return dvd2;
-            default:return dvd3;
+            case 2:return dvd3;
+            case 3:return cd1;
+            case 4:return cd2;
+            default: return cd3;
         }
     }
     public static void assistirFilme(){
